5e5ed721eeba66ddb899143429004f656186b74f
User <User@DESKTOP-2RC2UP5>
2019-09-25 09:06:52 +0700

    first commit

---
+++ src/pbo2/pkg10118055/latihan15/logika/PBO210118055Latihan15Logika.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118055.latihan15.logika;
+
+/**
+ *
+ * @author User
+ * NAMA:REZA PRATAMA
+ * KELAS:PBO2
+ * NIM:10118055
+ * Deskripsi:program ini berisi program untuk menampilkan
+ * jenis jenis data bilangan bulat
+ */
+public class PBO210118055Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a =true;
+        boolean b =false;
+        System.out.println("a && b ="+(a&&b));
+        System.out.println("a || b ="+(a||b));
+        System.out.println("!a ="+ !a);
+        System.out.println("!(a && b) ="+!(a&&b));
+    }
+    
+}
